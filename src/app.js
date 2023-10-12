8db738208c792b6709287e00097837ec7df2a028
Gabriel Ossa <125922133+gossae101@users.noreply.github.com>
2023-10-12 12:59:33 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["My dog ", "My grandma ", "His turtle ", "My bird "];
+  let action = ["ate ", "peed on ", "crushed ", "broke "];
+  let what = ["my homework ", "the keys ", "the car "];
+  let when = [
+    "before the class.",
+    "right on time.",
+    "when I finished.",
+    "during my lunch.",
+    "while I was praying."
+  ];
+
+  var rdm1 = Math.floor(Math.random() * who.length);
+  var rdm2 = Math.floor(Math.random() * action.length);
+  var rdm3 = Math.floor(Math.random() * what.length);
+  var rdm4 = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[rdm1] + action[rdm2] + what[rdm3] + when[rdm4];
 };
